77950e280640918fd47d1d5526b45b0d1ceb2c46
karlaem <60242444+karlaem@users.noreply.github.com>
2021-04-16 18:34:22 -0700

    added fonts and header

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!--meta description-->
+<meta charset="UTF-8">
+<meta name="description" content="Karla Mercado Web Developer Portfolio">
+<meta name="keywords" content="web development, design, art, animation, media, coding, digital media">
+<meta name="author" content="Karla Mercado">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--css-->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/darkmode.css">
+
+
+<!--fonts-->
+<link rel="stylesheet" href="css/fonts/stylesheet.css" type="text/css" charset="utf-8" />
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Antic+Didone&display=swap" rel="stylesheet">
+
+<!--favicon-->
+<link rel="apple-touch-icon" sizes="180x180" href="imgs/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="imgs/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="imgs/favicon/favicon-16x16.png">
+<link rel="manifest" href="imgs/favicon/site.webmanifest">
+<link rel="mask-icon" href="imgs/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<!--jquery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lettering.js/0.7.0/jquery.lettering.min.js"></script> 
+
+<!--google analytics-->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P84GGEBBBM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P84GGEBBBM');
+</script>
+
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Antic+Didone&display=swap" rel="stylesheet">
-    
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/darkmode.css">
-    <title>Document</title>
+<?php
+    // links
+    include("includes/header.php");
+    ?>
+    <title>Karla Mercado Web Developer and Front End Designer Portfolio Home</title>
 </head>
 <body> 
     <div class="container">
