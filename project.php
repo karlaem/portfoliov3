dc2d7a9c3c9be234a64603da00e37da734b01cc6
karlaem <60242444+karlaem@users.noreply.github.com>
2021-04-18 19:37:16 -0700

    notfound

--- project.php
+++ project.php
@@ -9,32 +9,18 @@
 </head>
 <body>
 
-<div id="myWebApp" class="marginleft phonetop">
-        <div id="project" class="page">
-        <div class="comingsoon">
 
-            <div  class="content">                
-         
-                    <section class="container hide">
-                        <h1>
-                        <span class="title">Coming</span>
-                        <span class="title"> soon...</span>
-                        </h1>
-                        
-                    </section>
-                    <h1 class="title2">Coming soon...</h1>
-                </div>
-            </div>
+<div id="project" class="page">
+    <div class="comingsoon">
+        <div  class="textcontent">                
+            <h1 class="title2">Coming soon...</h1>
         </div>
+    </div>
+
+    <?php
+    // Navigation
+    include("includes/nav.php");
+    ?>   
 </div> 
-<?php
-// Navigation
-include("includes/nav.php");
-?>   
-<div class="decorfooter">
-<?php
-// footer
-include("includes/footer.php");
-?>    
 </body>
 </html>
